9748976a256fb7c8ee617f076f1018c672d57cf6
Rafael Monico <rmonico@moninburgo>
2019-06-24 18:28:45 -0300

    Database commands moved to classes

--- fdc/command/db.py
+++ fdc/command/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 class DBCommand(object):
 
     def make_parser(parent_parser):
@@ -10,20 +9,27 @@ class DBCommand(object):
 
         subparsers.add_parser(
             "init", help="Inicializa o banco de dados em fdc.db").set_defaults(clazz=DBInit)
-        # subparsers.add_parser("restore", help="Restore fdc.dump to fdc.db").set_defaults(
-        #     command=self.restore)
-        # subparsers.add_parser(
-        #     "dump", help="Dump fdc.db to fdc.dump").set_defaults(command=self.dump)
 
-    def restore(self, args):
-        # Usar iterdump do sqlite3
-        print("in db restore {}".format(args))
-
-    def dump(self, args):
-        print("in db dump {}".format(args))
+        subparsers.add_parser("restore", help="Restore fdc.dump to fdc.db").set_defaults(
+            command=DBRestore)
+        subparsers.add_parser(
+            "dump", help="Dump fdc.db to fdc.dump").set_defaults(command=DBDump)
 
 
 class DBInit(object):
 
     def run(self, args):
         print("in db init {}".format(args))
+
+
+class DBRestore(object):
+
+    def run(self, args):
+        # Usar iterdump do sqlite3
+        print("in db restore {}".format(args))
+
+
+class DBDump(object):
+
+    def run(self, args):
+        print("in db dump {}".format(args))
