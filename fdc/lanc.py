9b2acb56a4ff91972339072bfcaf74d840419f84
Rafael Monico <rmonico@moninburgo>
2019-06-25 10:43:22 -0300

    Removed unused module

--- fdc/lanc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-class Add(object):
-
-    def run(self, args):
-        print('origem: {}'.format(args.origem))
-        print("in lanc add {}".format(args))
