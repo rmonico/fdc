df659e6a81bac9672dee8b0319d0a67f2db57fb8
Rafael Monico <rmonico1@gmail.com>
2019-06-28 14:55:38 -0300

    classvisitor working

--- fdc/classvisitor.py
+++ fdc/classvisitor.py
@@ -2,21 +2,28 @@
 # -*- coding: utf-8 -*-
 
 
+def has_method(clazz, method_name):
+    import inspect
+    return hasattr(clazz, method_name) and inspect.isfunction(getattr(clazz, method_name))
+
+
 class ClassVisitor(object):
 
     _classes = []
     _modules = []
 
-    def __init__(self, sub_module):
+    def __init__(self, sub_module, filter=lambda clazz: True):
         self._sub_module = sub_module
+        self._filter = filter
 
     def _load_classes(self):
-        if self._sub_module in self._modules:
+        if self._sub_module in ClassVisitor._modules:
             return
 
         import importlib
 
-        command_module = importlib.import_module("." + self._sub_module, __package__)
+        command_module = importlib.import_module(
+            "." + self._sub_module, __package__)
 
         import pkgutil
         import inspect
@@ -30,9 +37,27 @@ class ClassVisitor(object):
                     classes.append(clazz)
                     break
 
-        return classes
+        ClassVisitor._classes += classes
+        ClassVisitor._modules += [self._sub_module]
 
-    def visit(self, client):
-        for clazz in self._classes:
-            if str(clazz.__module__) == self._sub_module:
-                client(clazz)
+    def _parent_module_name(self, module):
+        module_name = str(module)
+
+        return module_name[:module_name.rfind(".")]
+
+    def _is_at_sub_module(self, clazz):
+        clazz_parent_module_name = self._parent_module_name(clazz.__module__)
+
+        self_parent_module_name = self._parent_module_name(self.__module__)
+
+        target_module_name = self_parent_module_name + "." + self._sub_module
+
+        return target_module_name == clazz_parent_module_name
+
+    def visit(self, visitor):
+        self._load_classes()
+
+        for clazz in ClassVisitor._classes:
+            if self._is_at_sub_module(clazz):
+                if self._filter(clazz):
+                    visitor(clazz)
