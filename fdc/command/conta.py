49ae93121e5de1139449dac9d9db8dd1381b78fc
Rafael Monico <rmonico1@gmail.com>
2019-07-07 20:59:43 -0300

    Refactor

--- fdc/command/conta.py
+++ fdc/command/conta.py
@@ -7,8 +7,8 @@ from fdc.dao.conta import Conta, ContaDao
 
 class ContaCommand(object):
 
-    def root_parser_created_handler(self, parent_parser):
-        conta_parser = parent_parser.add_parser(
+    def root_parser_created_handler(self, root_parser):
+        conta_parser = root_parser.add_parser(
             "conta", help="Comandos de conta")
         subparsers = conta_parser.add_subparsers()
 
