d05e0344ea1bff0582c86c24a5b7e4335ab17eb7
Rafael Monico <rmonico1@gmail.com>
2019-07-07 22:03:56 -0300

    Controller self injecting resources, logging loaded listeners

--- di_container/controller.py
+++ di_container/controller.py
@@ -3,6 +3,7 @@
 
 
 from .methodvisitor import MethodVisitor
+from .injector import di_container
 
 
 class Controller(object):
@@ -29,6 +30,11 @@ class Controller(object):
         visitor = MethodVisitor(packages, lambda clazz, method: method.__name__.endswith('_handler'))
 
         visitor.visit(self._load_listeners_from)
+
+        di_container.inject_resources(self)
+
+        for listener in self._listeners:
+            self._logger.info('Loaded listener: {}', listener[0].__qualname__)
 
     def event(self, event_name, *args, **kwargs):
         for listener, instance in self._listeners:
