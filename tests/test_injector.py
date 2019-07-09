f0b97ed1fc3d494c318d54ed099e27f7ce76227c
Rafael Monico <rmonico1@gmail.com>
2019-07-09 19:48:53 -0300

    Testing external dependency injection (not passing yet)

--- tests/test_injector.py
+++ tests/test_injector.py
@@ -18,14 +18,40 @@ class InjectorTestCase(TestCase):
 
         self.assertIsInstance(injected._dependency, DependencyToBeInjected)
 
+    def test_external_dependency_injection(self):
+        di_container.load_resources([__package__])
+
+        injected = Injected()
+
+        di_container.inject_resources(injected)
+
+        self.assertEqual(injected._external_dependency, 'external dependency instance')
+
 
 class Injected(object):
+
+    def __init__(self):
+        self._dependency = None
+        self._external_dependency = None
 
     def set_dependency_name(self, dependency):
         self._dependency = dependency
 
+    def set_external_dependency(self, dependency):
+        self._external_dependency = dependency
+
 
 class DependencyToBeInjected(object):
 
+    @staticmethod
     def injectable_resource():
         return 'dependency_name'
+
+    @staticmethod
+    def get_external_resources():
+        return [{'name': 'external_dependency',
+                 'creator': DependencyToBeInjected.callable_who_will_return_dependency_instance_when_injected}]
+
+        @staticmethod
+        def callable_who_will_return_dependency_instance_when_injected():
+            return 'external dependency instance'
