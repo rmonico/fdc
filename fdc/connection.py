eefa27148714c5eeb24c8fe0194ef6c5d7f10651
Rafael Monico <rmonico1@gmail.com>
2019-07-11 15:40:28 -0300

    Extracted a Configurations class from ConnectionFactory

--- fdc/connection.py
+++ fdc/connection.py
@@ -5,33 +5,67 @@
 import sqlite3
 
 
-class ConnectionFactory(object):
+class Configurations(object):
+
+    def __init__(self):
+        self._logger = None
 
     @staticmethod
-    def injectable_resource():
-        return 'connection_factory'
+    def get_external_resources():
+        return [{'name': 'configs', 'creator': Configurations._load_configurations}]
 
     def set_logger(self, logger):
         self._logger = logger
 
-    @staticmethod
-    def database_folder():
+    def _visit_configs(self, visitor):
+        self._visit_config(None, self._configs, visitor)
+
+    def _visit_config(self, parent_key, configs, visitor):
+        for key, value in configs.items():
+            if parent_key:
+                qualified_key = parent_key + '.' + key
+            else:
+                qualified_key = key
+
+            if isinstance(value, dict):
+                self._visit_config(qualified_key, value, visitor)
+            else:
+                visitor(qualified_key, value)
+
+    def _load_configurations(self):
+        # TODO Load this from file
         import os
 
-        return "{HOME}/.config/fdc".format(**os.environ)
+        db_folder = '{HOME}/.config/fdc'.format(**os.environ)
+        db_path = '{}/database.db'.format(db_folder)
+
+        self._configs = {'db': {'folder': db_folder, 'path': db_path}}
+
+        self._logger.info('Loaded configs:')
+
+        self._visit_configs(lambda key, value: self._logger.info('{}={}'.format(key, value)))
+
+        return self._configs
+
+
+class ConnectionFactory(object):
+
+    def __init__(self):
+        self._configs = None
 
     @staticmethod
-    def database_path():
-        # TODO CHeck this in other places (like environment variables, command
-        # line switches, etc)
-        return "{}/database.db".format(Factory.database_folder())
+    def get_external_resources():
+        return [{'name': 'connection', 'creator': ConnectionFactory.create_connection}]
 
-    def create_connection():
+    def set_configs(self, configs):
+        self._configs = configs
+
+    def create_connection(self):
         import os
 
-        os.makedirs(Factory.database_folder(), exist_ok=True)
+        os.makedirs(self._configs['db']['folder'], exist_ok=True)
 
-        return sqlite3.connect(Factory.database_path())
+        return sqlite3.connect(self._configs['db']['path'])
 
 
 def execute(sql, parameters=None, commit_and_close=False):
