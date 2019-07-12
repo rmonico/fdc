2127910fb2356d4481af2a43788c5323a0cfbdeb
Rafael Monico <rmonico1@gmail.com>
2019-07-11 23:49:36 -0300

    Database initialization command working

--- fdc/command/db.py
+++ fdc/command/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from di_container.injector import di_container
+
 
 class DBCommand(object):
 
-    def root_parser_created_event_handler(self, parent_parser):
-        db_parser = parent_parser.add_parser("db", help="Database commands")
+    def root_parser_created_handler(self, root_parser):
+        db_parser = root_parser.add_parser("db", help="Database commands")
         subparsers = db_parser.add_subparsers()
 
         subparsers.add_parser(
@@ -19,23 +21,30 @@ class DBCommand(object):
 
 class DBInit(object):
 
+    def __init__(self):
+        self._configs = None
+
+    def set_configs(self, configs):
+        self._configs = configs
+
     def run(self, args):
         import os
 
-        os.remove(Factory.database_path())
+        if os.path.exists(self._configs['db']['path']):
+            os.remove(self._configs['db']['path'])
 
-        conn = Factory.create_connection()
+        connection = di_container.get_resource('connection')
 
         # TODO Move this to ContaCommand class (may that class should be
         # renamed)
-        conn.executescript(
+        connection.executescript(
             "create table conta (nome text not null, contabilizavel boolean not null, fechamento date);")
 
         # FIXME Não armazenar monetários como float
-        conn.executescript(
+        connection.executescript(
             "create table contrato (compra date not null, conta, total_parcelas integer, valor_parcela float, observacao text, foreign key(conta) references conta);")
 
-        conn.executescript(
+        connection.executescript(
             "create table lancamento (debito date not null, compra date, valor float, valor_local float not null, origem, destino, parcela integer not null, observacao text not null, foreign key(origem) references conta, foreign key(destino) references conta);")
 
         # TODO Commit and handle errors
