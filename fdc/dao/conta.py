c8972d5acae6785db32cac491464a5cc660f5fa8
Rafael Monico <rmonico1@gmail.com>
2019-07-14 22:25:25 -0300

    ContaDao list method

--- fdc/dao/conta.py
+++ fdc/dao/conta.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class Conta():
-    pass
+_fields = "nome", "contabilizavel", "fechamento"
 
 
-class ContaDao():
+class Conta(object):
+
+    def __init__(self, conta_row):
+        for field in _fields:
+            setattr(self, '_' + field, conta_row[field])
+
+
+class ContaDao(object):
+
+    def __init__(self):
+        self._connection = None
+
+    @staticmethod
+    def injectable_resource():
+        return 'conta_dao'
+
+    def set_connection(self, connection):
+        self._connection = connection
+
+    def list(self):
+        cursor = self._connection.execute(
+            "select {fields} from conta;".format(fields=", ".join(_fields)))
+
+        conta_list = []
+
+        for row in cursor:
+            conta = Conta(conta_row=row)
+
+            conta_list.append(conta)
+
+        return conta_list
 
     def insert(self, conta):
         # TODO Extract these code to a InsertBuilder class (maybe on connection
