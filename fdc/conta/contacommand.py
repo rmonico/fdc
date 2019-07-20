3c20c57839487a1dc819c495e321f340781bf602
Rafael Monico <rmonico1@gmail.com>
2019-07-20 17:18:07 -0300

    Alterado apelido do comando conta

--- fdc/conta/contacommand.py
+++ fdc/conta/contacommand.py
@@ -8,7 +8,7 @@ class ContaCommand(object):
 
     def root_parser_created_handler(self, root_parser):
         conta_parser = root_parser.add_parser(
-            "conta", aliases=['c', 'ct', 'cta'], help="Comandos de conta")
+            "conta", aliases=['ct'], help="Comandos de conta")
         subparsers = conta_parser.add_subparsers()
 
         controller.event('conta_parser_created', conta_parser=subparsers)
