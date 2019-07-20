f2096317ac5f4b21cb2c5b605f11002713774bd7
Rafael Monico <rmonico1@gmail.com>
2019-07-20 20:34:40 -0300

    Testing new model of controller results returning

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -20,6 +20,14 @@ class TestEventHandler(object):
         _test_event_parameter = 41
 
 
+class EventHandlerWillReturnData(object):
+
+    def event_that_will_return_data_handler(self):
+        return 'status', {'property1': 'key-value data 1 value',
+                'property2': 'software é como lingüiça: melhor não saber como é feito'}, 'first data', \
+               'second data'
+
+
 class ControllerTestCase(TestCase):
 
     def test_event_triggered(self):
@@ -43,6 +51,22 @@ class ControllerTestCase(TestCase):
         controller.event('parametrized_event', parameter=41)
 
         self.assertEqual(_test_event_parameter, 41)
+
+    def test_event_which_return_data(self):
+        di_container.load_resources(['commons'])
+        controller.load_listeners([__package__])
+
+        results = controller.event('event_that_will_return_data')
+
+        self.assertIsInstance(results, list)
+
+        self.assertEqual(len(results), 1)
+
+        result = results[0]
+        self.assertEqual(result.status, 'status')
+        self.assertEqual(result.data, ('first data', 'second data'))
+        self.assertEqual(result.kwdata['property1'], 'key-value data 1 value')
+        self.assertEqual(result.kwdata['property2'], 'software é como lingüiça: melhor não saber como é feito')
 
 
 if __name__ == '__main__':
