2296d169447daa2db1508ade3dce01bc2a8a9d0b
Rafael Monico <rmonico1@gmail.com>
2019-07-20 22:06:52 -0300

    Logger: Loading verbosity from configurations, implemented methods for each level

--- commons/logger.py
+++ commons/logger.py
@@ -1,17 +1,32 @@
-
-_NONE = 0
-_INFO = 1
+from di_container.injector import Inject
 
 
 class Logger(object):
+    _verbosity_level = {'NONE': 0, 'WARN': 1, 'INFO': 2, 'DEBUG': 3}
 
     def __init__(self):
-        self._verbosity_level = _NONE
+        self._configs = Inject('app configuration')
+        self._verbosity_level = None
 
     @staticmethod
     def injectable_resource():
         return 'logger'
 
+    def message(self, message, *args, **kwargs):
+        self._log(message, Logger._verbosity_level['NONE'], *args, **kwargs)
+
+    def warn(self, message, *args, **kwargs):
+        self._log(message, Logger._verbosity_level['WARN'], *args, **kwargs)
+
     def info(self, message, *args, **kwargs):
-        if self._verbosity_level >= _INFO:
+        self._log(message, Logger._verbosity_level['INFO'], *args, **kwargs)
+
+    def debug(self, message, *args, **kwargs):
+        self._log(message, Logger._verbosity_level['DEBUG'], *args, **kwargs)
+
+    def _log(self, message, level, *args, **kwargs):
+        if not self._verbosity_level:
+            self._verbosity_level = Logger._verbosity_level[self._configs['log.verbosity']]
+
+        if self._verbosity_level >= level:
             print(message.format(*args, **kwargs))
