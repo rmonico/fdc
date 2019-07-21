2775be6536e9f55ce13ff478a29239063bd3676e
Rafael Monico <rmonico1@gmail.com>
2019-07-20 21:01:59 -0300

    Module renamed

--- commons/configurations.py
+++ commons/configurations.py
@@ -3,12 +3,32 @@ from di_container.injector import Inject
 
 class Configurations(object):
 
+    def __init__(self, configs):
+        self._configs = configs
+
+    def get(self, qualified_key):
+        return 'value'
+
+    def _get(self, value, remaining_key):
+        if isinstance(value, dict):
+            dot_index = remaining_key.index('.')
+
+
+            first_key = remaining_key[0:dot_index]
+            subvalue = value[first_key]
+            return self._get()
+        else:
+            return value
+
+
+class ConfigurationFactory(object):
+
     def __init__(self):
         self._logger = Inject('logger')
 
     @staticmethod
     def get_external_resources():
-        return [{'name': 'app configuration', 'creator': Configurations._load_configurations}]
+        return [{'name': 'app configuration', 'creator': ConfigurationFactory._load_configurations}]
 
     def _visit_configs(self, visitor):
         self._visit_config(None, self._configs, visitor)
@@ -32,7 +52,7 @@ class Configurations(object):
         db_folder = '{HOME}/.config/fdc'.format(**os.environ)
         db_path = '{}/database.db'.format(db_folder)
 
-        self._configs = {'db': {'folder': db_folder, 'path': db_path}}
+        self._configs = {'db': {'folder': db_folder, 'path': db_path}, 'log': {'verbosity': 'INFO'}}
 
         self._logger.info('Loaded configs:')
 
