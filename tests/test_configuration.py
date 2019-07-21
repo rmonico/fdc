b90495eac07cd02000e8c5b14c15b4275f4ccdea
Rafael Monico <rmonico1@gmail.com>
2019-07-20 21:31:52 -0300

    Configuration: Tests for more complex keys

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -9,3 +9,12 @@ class TestConfiguration(TestCase):
         c = Configurations({'key': 'value'})
 
         self.assertEqual(c.get('key'), 'value')
+
+    def test_get_with_subkeys(self):
+        c = Configurations({'key': {'keys first subkey': 'subkey value', 'anothersubkey': 'another sub key value', 'subkey': {'first subsubkey': 'value of first sub sub key'}}})
+
+        self.assertIsInstance(c.get('key'), dict)
+        self.assertEqual(c.get('key.keys first subkey'), 'subkey value')
+        self.assertEqual(c.get('key.anothersubkey'), 'another sub key value')
+        self.assertIsInstance(c.get('key.subkey'), dict)
+        self.assertEqual(c.get('key.subkey.first subsubkey'), 'value of first sub sub key')
