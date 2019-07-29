441e77a34cb1e6492d06395e0533924ecf0c9fd6
Rafael Monico <rmonico1@gmail.com>
2019-07-28 21:18:41 -0300

    TODO removed

--- fdc/commons/connection_factory.py
+++ fdc/commons/connection_factory.py
@@ -16,7 +16,6 @@ class ConnectionFactory(object):
     def create_connection(self):
         import os
 
-        # TODO Is using a project specific configuration key, extract this class to fdc.commons
         connection = sqlite3.connect(self._configs['fdc.db_full_path'])
 
         self._logger.debug('Connected to database at {}', self._configs['fdc.db_full_path'])
