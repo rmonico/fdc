81bd896bfcf8822964255149acece68987fc64fb
Rafael Monico <rmonico1@gmail.com>
2019-07-28 21:42:36 -0300

    Database dump coded

--- fdc/database/databasedumpcommand.py
+++ fdc/database/databasedumpcommand.py
@@ -1,9 +1,23 @@
+from di_container.injector import Inject
+
+
 class DatabaseDumpCommand(object):
 
+    def __init__(self):
+        self._logger = Inject('logger')
+        self._connection = Inject('database connection')
+        self._config = Inject('app configuration')
+
     def database_parser_created_handler(self, db_parser):
-        db_parser.add_parser("dump", help="Dump fdc.db to fdc.dump").set_defaults(event='database_dump_command')
+        db_parser.add_parser("dump", help="Dump database.db top database.dump").set_defaults(
+            event='database_dump_command')
 
     def database_dump_command_handler(self, args):
-        print('TODO')
+        with open(self._config['dump.full_path'], 'w') as file:
+            for line in self._connection.iterdump():
+                file.write('%s\n' % line)
 
         return 'ok'
+
+    def database_dump_command_ok_handler(self):
+        print('Database contents dumped')
