0e095ddebe930666de6e893878afed5966e64d1a
Rafael Monico <rmonico1@gmail.com>
2019-07-28 21:42:19 -0300

    Configurations: Fixed error on database full path mounting, code formatted

--- fdc/commons/configuration_factory.py
+++ fdc/commons/configuration_factory.py
@@ -14,9 +14,20 @@ class ConfigurationFactory(object):
 
         fdc_folder = '{HOME}/.config/fdc'.format(**os.environ)
         db_file_name = 'database.db'
+        dump_file_name = 'dump.db'
 
         log_level = 'DEBUG'
 
         return Configurations(
-            {'fdc': {'folder': fdc_folder, 'db_file_name': db_file_name, 'db_full_path': fdc_folder + db_file_name},
-             'log': {'verbosity': log_level}})
+            {
+                'fdc': {
+                    'folder': fdc_folder,
+                    'db_file_name': db_file_name,
+                    'db_full_path': fdc_folder + '/' + db_file_name
+                },
+                'dump': {
+                    'file_name': dump_file_name,
+                    'full_path': fdc_folder + '/' + dump_file_name
+                },
+                'log': {'verbosity': log_level}
+            })
