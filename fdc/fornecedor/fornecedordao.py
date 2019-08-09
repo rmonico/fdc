947601196b4d01cf4cbbcebe1749334343a4a1da
Rafael Monico <rmonico1@gmail.com>
2019-08-08 22:42:21 -0300

    FornecedorDao ported to new structure

--- fdc/fornecedor/fornecedordao.py
+++ fdc/fornecedor/fornecedordao.py
@@ -1,10 +1,19 @@
-from di_container.injector import Inject
+from commons.abstract_dao import AbstractDao
+from commons.sqlbuilder import TableDescriptor
+
+fornecedor_table_descriptor = TableDescriptor('fornecedor', 'rowid', 'nome')
 
 
-class FornecedorDao(object):
+class Fornecedor(object):
 
     def __init__(self):
-        self._connection = Inject('database connection')
+        fornecedor_table_descriptor.create_field_attributes(self)
+
+
+class FornecedorDao(AbstractDao):
+
+    def __init__(self):
+        super().__init__(Fornecedor, fornecedor_table_descriptor)
 
     @staticmethod
     def injectable_resource():
