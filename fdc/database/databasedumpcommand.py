efb5f5de470fd00785b8302ab3747a1162924c25
Rafael Monico <rmonico1@gmail.com>
2019-09-29 18:52:26 -0300

    Database dump command: Passing event parameter again back to caller

--- fdc/database/databasedumpcommand.py
+++ fdc/database/databasedumpcommand.py
@@ -9,7 +9,10 @@ class DatabaseDumpCommand(object):
         self._config = Inject('app configuration')
 
     def database_parser_created_handler(self, db_parser):
-        db_parser.add_parser("dump", help="Dump database.db to database.dump")
+        db_parser.add_parser("dump", help="Dump database.db to database.dump").set_defaults(
+            event='database_dump_command')
+
+        # TODO Flag to skip specific git steps (or all of then)
 
     def database_dump_command_handler(self, args):
         with open(self._config['fdc.dump_full_path'], 'w') as file:
