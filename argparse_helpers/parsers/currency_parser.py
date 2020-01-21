dba23a778e11910c51b13edcefe1ae7671ae448f
Rafael Monico <rmonico@moninvostok>
2020-01-21 21:05:48 +0000

    currency_parser: Improved message on invalid values

--- argparse_helpers/parsers/currency_parser.py
+++ argparse_helpers/parsers/currency_parser.py
@@ -3,6 +3,6 @@ import decimal, re, argparse
 
 def currency_parser(s):
     if not re.match('^[0-9]+\.[0-9]{2}$', s):
-        raise argparse.ArgumentTypeError('Invalid currency: "{}"'.format(s))
+        raise argparse.ArgumentTypeError('Invalid currency value: "{}" (should be in format 0.00)'.format(s))
 
     return decimal.Decimal(s)
