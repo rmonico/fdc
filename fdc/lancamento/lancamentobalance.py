6ac6be02f7e6de2084e0d6072b8077d81e1526ec
Rafael Monico <rmonico@moninburgo>
2020-09-26 22:18:58 -0300

    refactor: "lanc balance" command handling moved to its own class

--- fdc/lancamento/lancamentobalance.py
+++ fdc/lancamento/lancamentobalance.py
@@ -1,45 +1,16 @@
-from commons.tableprinter import TablePrinter
-from di_container.injector import Inject
-
-
 class LancamentoBalance(object):
 
-    def __init__(self):
-      self._dao = Inject('lancamento dao')
-
     def lancamento_parser_created_handler(self, lancamento_parser):
-      parser = lancamento_parser.add_parser('balance', help='Lists the balance evolution day by day')
+        parser = lancamento_parser.add_parser('balance', help='Lists the balance evolution day by day')
 
-      parser.set_defaults(event='lancamento_balance_command')
+        parser.set_defaults(event='lancamento_balance_command')
 
-    def lancamento_balance_command_handler(self, args):
-      # TODO Limit this by date
-      lancamentos_to_work_on = self._dao.list()
-
-      lancamentos_por_dia = self.group_lancamentos_per_day(lancamentos_to_work_on)
-
-      saldos = {}
-
-      for data, lancamentos in lancamentos_por_dia.items():
-        saldo = {}
-        for lancamento in lancamentos:
-          saldo.ge
-
-        saldos.set_default(data, saldo)
-
-      return 'ok', {'saldos': saldos}
-
-    def group_lancamentos_per_day(self, lancamentos):
-      lancamentos_per_day = {}
-
-      for lancamento in lancamentos:
-        day_lancamentos = lancamentos_per_day.set_default(lancamento.data, [])
-
-        day_lancamentos.append(lancamento)
-
-      return lancamentos_per_day
 
     def lancamento_balance_command_ok_handler(self, saldos):
-      printer = TablePrinter(self._dao._metadata.fields, saldos)
+        # printer = TablePrinter(self._lancamento_dao._metadata.fields, saldos)
 
-      printer.print()
+        # printer.print()
+
+        for data, saldo in sorted(saldos.items()):
+            print('{} - {}'.format(data, saldo))
+
---
+++ fdc/lancamento/lancamentobalancehandler.py
@@ -0,0 +1,61 @@
+from commons.tableprinter import TablePrinter
+from di_container.injector import Inject
+from decimal import Decimal
+from collections import OrderedDict
+
+
+class LancamentoBalanceHandler(object):
+
+    def __init__(self):
+        self._lancamento_dao = Inject('lancamento dao')
+        self._conta_dao = Inject('conta dao')
+
+    def lancamento_balance_command_handler(self, args):
+        # TODO Limit this by date
+        lancamentos_to_work_on = self._lancamento_dao.list()
+    
+        for lancamento in lancamentos_to_work_on:
+            self._load_conta_data(lancamento)
+
+        lancamentos_per_day = self._group_lancamentos_per_day(lancamentos_to_work_on)
+
+        saldos = dict()
+
+        for data, lancamentos in lancamentos_per_day.items():
+            saldos_do_dia = dict()
+
+            for lancamento in lancamentos:
+                self._update_balance(saldos_do_dia, lancamento.origem, lancamento.valor)
+                self._update_balance(saldos_do_dia, lancamento.destino, lancamento.valor)
+
+            saldos[data] = saldos_do_dia
+
+        return 'ok', {'saldos': saldos}
+
+    def _load_conta_data(self, lancamento):
+        lancamento.origem = self._conta_dao.get_single('rowid = ?', lancamento.origem)
+        lancamento.destino = self._conta_dao.get_single('rowid = ?', lancamento.destino)
+
+    def _group_lancamentos_per_day(self, lancamentos):
+        lancamentos_per_day = {}
+
+        for lancamento in lancamentos:
+            day_lancamentos = lancamentos_per_day.setdefault(lancamento.data, [])
+
+            day_lancamentos.append(lancamento)
+
+        return lancamentos_per_day
+
+    def _update_balance(self, saldos_do_dia, conta, valor):
+        # TODO Continuar aqui, precisa somar ou subtrair conforme origem, destino ou saldo
+        # TODO Também deve acumular por dia
+        # Talvez mover essa lógica para uma classe que faça apenas isso. Um classe que recebe uma lista de lancamentos e devolve cada um com uma tupla extra com os saldos da origem e do destino, ou ainda um dict com as contas e os respectivos saldos após o lançamento
+        # Talvez essa lógica deva ir para o lanc ls num parâmetro --balance
+        if 'contabilizável' in conta.propriedades:
+            saldo = saldos_do_dia.setdefault(conta.nome, Decimal(0))
+
+            saldo += valor / Decimal(100)
+
+            saldos_do_dia[conta.nome] = saldo
+
+
