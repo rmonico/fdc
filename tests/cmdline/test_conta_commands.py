4f0d9366edb69d4ccef629b0f735d656f0b24666
Rafael Monico <rmonico@moningrado>
2020-12-07 04:54:19 -0300

    [conta_tests] setUp method extracted

--- tests/cmdline/test_conta_commands.py
+++ tests/cmdline/test_conta_commands.py
@@ -3,20 +3,19 @@ from . base_command_test_case import BaseCommandTestCase
 
 class ContaCommandsTests(BaseCommandTestCase):
 
-    def test_conta_add_should_create_new_conta(self):
-        self._call_fdc('db', 'init', for_command='conta add')
+    def setUp(self):
+        super().setUp()
+        self._call_fdc('db', 'init')
 
+    def test_conta_add_should_create_new_conta(self):
         self._call_fdc('conta', 'add', 'conta_teste')
 
         with self.runsql('select rowid, nome from conta;') as rs:
             self.assertResultSet(rs, (1, 'conta_teste'))
 
     def test_conta_list_should_list_contas(self):
-        self._call_fdc('db', 'init', for_command='conta list')
-
-        self._call_fdc('conta', 'add', 'conta_1', for_command='conta list')
-
-        self._call_fdc('conta', 'add', 'conta_2', for_command='conta list')
+        self._call_fdc('conta', 'add', 'conta_1')
+        self._call_fdc('conta', 'add', 'conta_2')
 
         stdout = self._call_fdc('conta', 'list')
 
