2ea2c29a1d6e6e2a139320fbf2f981199bda2a7a
Rafael Monico <rmonico@moningrado>
2020-12-07 00:06:41 -0300

    [tests] Added commented tests

--- tests/cmdline/test_cmdline.py
+++ tests/cmdline/test_cmdline.py
@@ -83,10 +83,27 @@ class CommandLineTestCase(TestCase):
 
         database_filename = self._env('FDC_FOLDER') + '/main.db'
 
+    # def test_db_dump_should_dump_database_contents_on_file(self):
+    #     database_filename = self._env('FDC_FOLDER') + '/main.db'
+
+    #     conn = sqlite3.connect(database_filename)
+
+    #     rs = conn.execute("select * from sqlite_master where type='table';")
+
+    #     self._call_fdc('db', 'dump')
+    #     import ipdb; ipdb.set_trace()
+    #     # Checar o arquivo de dump gerado (em {HOME}/.config/fdc_tests/main.dump)
+    #     dump_file = open(self._env('FDC_FOLDER') + '/main.dump', 'r')
+
+    #     dump = dump_file.readlines()
+
+    #     self.assertEqual(dump[0], 'Dump contents')
+
+    #     dump_file.close()
+
+    # def test_db_dump_should_create_new_commit_with_dump_file(self):
+    #     # Checar as alterações no repositório do git
+    #     pass
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
