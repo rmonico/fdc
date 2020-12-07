9f6b0566c16cd3d6567d34a1a6ca1a7200454a05
Rafael Monico <rmonico@moningrado>
2020-12-07 05:10:00 -0300

    [conta_tests] contabilizavel test (failing)

--- tests/cmdline/test_conta_commands.py
+++ tests/cmdline/test_conta_commands.py
@@ -13,6 +13,12 @@ class ContaCommandsTests(BaseCommandTestCase):
         with self.runsql('select rowid, nome from conta;') as rs:
             self.assertResultSet(rs, (1, 'conta_teste'))
 
+    def test_conta_add_should_create_new_conta_contabilizavel(self):
+        self._call_fdc('conta', 'add', 'conta_teste', '--contabilizavel')
+
+        with self.runsql('select rowid, nome, propriedades from conta;') as rs:
+            self.assertResultSet(rs, (1, 'conta_teste', 'contabilizável'))
+
     def test_conta_list_should_list_contas(self):
         self._call_fdc('conta', 'add', 'conta_1')
         self._call_fdc('conta', 'add', 'conta_2')
