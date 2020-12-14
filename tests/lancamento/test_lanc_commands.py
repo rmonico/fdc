1e8e10251f75146a8500a4043d79bbd733faecfd
Rafael Monico <rmonico@moningrado>
2020-12-13 23:02:00 -0300

    [tests] lanc add (not passing)

--- tests/lancamento/test_lanc_commands.py
+++ tests/lancamento/test_lanc_commands.py
@@ -1,4 +1,5 @@
 from tests.highlevel_command_testcase import HighLevelCommandTestCase
+from datetime import date
 
 
 class LancCommandsTests(HighLevelCommandTestCase):
@@ -17,6 +18,15 @@ class LancCommandsTests(HighLevelCommandTestCase):
         stdout = self._call_fdc('lanc', 'list')
 
         self.assertWithRegexFile(stdout, __file__, 'expected_lanc_list_with_database_empty')
+
+    def test_lanc_add_should_add_lanc_with_just_origem_destino_and_valor(self):
+        self._call_fdc('conta', 'add', 'conta_origem')
+        self._call_fdc('conta', 'add', 'conta_destino')
+
+        self._call_fdc('lanc', 'add', 'conta_origem', 'conta_destino', '31.99')
+
+        with self.runsql('select data, origem, destino, valor from lancamento;') as rs:
+            self.assertResultSet(rs, (str(date.today()), 1, 2, 31.99))
 
 
 if __name__ == '__main__':
