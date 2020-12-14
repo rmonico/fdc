6866ffbddc178825ae312f81d1eae95d5a67ce96
Rafael Monico <rmonico@moningrado>
2020-12-13 22:43:16 -0300

    [tests] Removed comment

--- tests/lancamento/test_lanc_commands.py
+++ tests/lancamento/test_lanc_commands.py
@@ -3,12 +3,6 @@ from tests.highlevel_command_testcase import HighLevelCommandTestCase
 
 class LancCommandsTests(HighLevelCommandTestCase):
 
-    # def test_lanc_add_should_create_new_conta(self):
-    #     self._call_fdc('conta', 'add', 'conta_teste')
-
-    #     with self.runsql('select rowid, nome from conta;') as rs:
-    #         self.assertResultSet(rs, (1, 'conta_teste'))
-
     def test_lanc_list_should_list_lancs(self):
         self._call_fdc('conta', 'add', 'conta_origem')
         self._call_fdc('conta', 'add', 'conta_destino')
@@ -17,8 +11,6 @@ class LancCommandsTests(HighLevelCommandTestCase):
 
         stdout = self._call_fdc('lanc', 'list')
 
-        # Problema: a data é variável
-        # Ideia: substituir com variáveis de ambiente no generator
         self.assertWithRegexFile(stdout, __file__, 'expected_lanc_list')
 
 
