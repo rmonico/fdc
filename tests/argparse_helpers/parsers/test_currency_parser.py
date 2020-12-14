51ae751c9a6714e75207b579b64b4559dbd2c8ae
Rafael Monico <rmonico@moningrado>
2020-12-13 23:09:40 -0300

    [tests] currency_parser tests (not passing)

--- tests/argparse_helpers/parsers/test_currency_parser.py
+++ tests/argparse_helpers/parsers/test_currency_parser.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
+from argparse_helpers.parsers.currency_parser import currency_parser
+from decimal import Decimal
+
 
 class CurrencyParserTestCase(TestCase):
-    def test_tests(self):
-        self.assertTrue(False)
+    def test_single_value_should_parse_to_decimal(self):
+        parsed = currency_parser('17.99')
+
+        self.assertEqual(type(parsed), Decimal)
+        self.assertEqual(parsed, Decimal('17.99'))
