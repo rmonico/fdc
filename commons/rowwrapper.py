93f29aa26337347874058012dec1a0401bcf999f
Rafael Monico <rmonico@moningrado>
2020-12-27 19:46:56 -0300

    tests - Refactor, rowwrapper._field_getter moved to its own class

--- commons/rowwrapper.py
+++ commons/rowwrapper.py
@@ -3,6 +3,27 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class ColumnWrapper(property):
+
+    def __init__(self, row_class, field_name, field_index, builder, consumed_itens):
+        super().__init__(self)
+        # self._row_class = row_class
+        # self._field_name = field_name
+        self._field_index = field_index
+        self._builder = builder
+        # self._consumed_itens = consumed_itens
+
+    def __call__(self, row_wrapper):
+        if self._builder:
+            if self._field_index not in row_wrapper._referenced_fields:
+                row_wrapper._referenced_fields[self._field_index] = self._builder(row_wrapper._row, self._field_index)
+
+            return row_wrapper._referenced_fields[row_wrapper._offset + self._field_index]
+
+        logger.debug('getting field index {} with offset {}'.format(self._field_index, row_wrapper._offset))
+        return row_wrapper._row[row_wrapper._offset + self._field_index]
+
+
 class RowWrapper(object):
     _field_count = 0
 
@@ -18,20 +39,9 @@ class RowWrapper(object):
                                                                                                        cls._field_count,
                                                                                                        builder,
                                                                                                        consumed_items))
-        field_index = int(cls._field_count)
-        p = property(lambda self: self._field_getter(field_index, builder))
+        p = ColumnWrapper(cls, name, cls._field_count, builder, consumed_items)
         setattr(cls, name, p)
         cls._field_count += consumed_items if consumed_items else getattr(builder, '_field_count', 1)
-
-    def _field_getter(self, field, builder):
-        if builder:
-            if field not in self._referenced_fields:
-                self._referenced_fields[field] = builder(self._row, field)
-
-            return self._referenced_fields[self._offset + field]
-
-        logger.debug('getting field index {} with offset {}'.format(field, self._offset))
-        return self._row[self._offset + field]
 
     @classmethod
     def load(cls, cursor):
