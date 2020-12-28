58138d6736281657a51908e52b145dd035ca774d
Rafael Monico <rmonico@moningrado>
2020-12-27 22:50:09 -0300

    tests - Recommit

--- commons/rowwrapper.py
+++ commons/rowwrapper.py
@@ -8,20 +8,17 @@ class ColumnWrapper(property):
     def __init__(self, row_class, field_name, field_index, builder, consumed_itens):
         super().__init__(self)
         # self._row_class = row_class
-        # self._field_name = field_name
+        self._field_name = field_name
         self._field_index = field_index
         self._builder = builder
         # self._consumed_itens = consumed_itens
 
     def __call__(self, row_wrapper):
-        if self._builder:
-            if self._field_index not in row_wrapper._referenced_fields:
-                row_wrapper._referenced_fields[self._field_index] = self._builder(row_wrapper._row, self._field_index)
+        # TODO Tem muitas referências à row_wrapper, mudar chamar um método de lá seja melhor
+        if self._field_index not in row_wrapper._referenced_fields:
+            row_wrapper._referenced_fields[self._field_index] = self._builder(row_wrapper._row, self._field_index)
 
-            return row_wrapper._referenced_fields[row_wrapper._offset + self._field_index]
-
-        logger.debug('getting field index {} with offset {}'.format(self._field_index, row_wrapper._offset))
-        return row_wrapper._row[row_wrapper._offset + self._field_index]
+        return row_wrapper._referenced_fields[row_wrapper._offset + self._field_index]
 
 
 class RowWrapper(object):
@@ -39,7 +36,11 @@ class RowWrapper(object):
                                                                                                        cls._field_count,
                                                                                                        builder,
                                                                                                        consumed_items))
-        p = ColumnWrapper(cls, name, cls._field_count, builder, consumed_items)
+        if not builder:
+            field_index = int(cls._field_count)
+            p = property(lambda self: self._row[self._offset + field_index])
+        else:
+            p = ColumnWrapper(cls, name, cls._field_count, builder, consumed_items)
         setattr(cls, name, p)
         cls._field_count += consumed_items if consumed_items else getattr(builder, '_field_count', 1)
 
--- commons/sqlbuilder.py
+++ commons/sqlbuilder.py
@@ -1,4 +1,5 @@
 import inspect
+from commons.rowwrapper import ColumnWrapper
 
 
 class TableDescriptor(object):
@@ -54,25 +55,49 @@ class SelectBuilder(object):
         self._entity_class = entity_class
 
     def build(self, where: str = None, fields: list = None):
-        # TODO Create a method to return this
-        table_name = self._entity_class.__name__.lower()
+        from_clause = self._get_from_clause()
 
-        _fields = fields if fields else self._get_fields(table_name)
+        _fields = fields if fields else self._get_fields(self._entity_class)
 
         _where = ' where {}'.format(where) if where else ''
 
-        return 'select {} from {}{};'.format(', '.join(_fields), table_name, _where)
+        return 'select {} from {}{};'.format(', '.join(_fields), from_clause, _where)
 
-    def _get_fields(self, table_name):
-        mro = inspect.getmro(self._entity_class)
+    def _get_from_clause(self):
+        # TODO Create a method to return this
+        main_table = self._entity_class.__name__.lower()
+        from_clause = main_table
+
+        parent_table = self._entity_class.__name__.lower()
+        for _joined_table in self._get_class_complex_fields(self._entity_class):
+            # FIXME Vai dar problema quando o builder não for RowWrapper
+            # FIXME Prever a situação que houver mais de um nível de join (deve chamar esse bloco recursivamente)
+            joined_table = getattr(self._entity_class, _joined_table)
+            joined_table_name = joined_table._builder.__name__.lower()
+            joined_table_field = joined_table._field_name
+
+            from_clause += ' left join {0} on ({1}.{2} = {0}.rowid)'.format(joined_table_name, parent_table, joined_table_field)
+
+        return from_clause
+
+    @staticmethod
+    def _get_fields(cls):
+        mro = inspect.getmro(cls)
         fields = list()
 
         for cls in reversed(mro):
+            # TODO Continuar mexendo aqui, precisa chamar o _get_fields recursivamente para montar os fields das tabelas joined
             fields.extend(SelectBuilder._get_class_fields(cls))
 
-        return [table_name + '.' + field for field in fields]
+        return [cls.__name__.lower() + '.' + field for field in fields]
 
     @staticmethod
     def _get_class_fields(cls):
         d = cls.__dict__
         return [p for p in d if type(d[p]) == property]
+
+    @staticmethod
+    def _get_class_complex_fields(cls):
+        d = cls.__dict__
+        return [p for p in d if type(d[p]) == ColumnWrapper]
+
